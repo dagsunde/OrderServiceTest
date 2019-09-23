86e6e1ecbc437c9ac7eda05ab605064af10284d4
Dag Sunde <dag.sunde@solver.no>
2019-09-23 15:20:45 +0200

    1st

---
+++ OrderServiceTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderServiceTest {
+    class Program {
+        static void Main(string[] args)
+        {
+            OrderServiceElsmartV3Client client = new OrderServiceElsmartV3Client();
+
+            // Bruk variabelen "client" til å kalle operasjoner på tjenesten.
+            var s = client.GetCallbackMsg("Hello!");
+
+            Console.Out.WriteLine(s);
+
+            var o = new orderservices.elsmart.com.v3.OrderRequest();
+            o.OrderHead = new orderservices.elsmart.com.v3.OrderHeadType();
+            o.OrderHead.ContractorPrefix = "999";
+            var r = client.UpdateWorkOrder(o);
+
+
+        }
+    }
+}
